38e04863230ae05d83f760e3102e13b50818060d
Jon Barrett <oicleaning@protonmail.com>
2025-08-02 11:53:43 -0700

    Initial blank sheet

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-stone-200">
+      {/* Character sheet goes here */}
+    </main>
+  );
+}
